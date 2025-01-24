dc30ccbed3f1771ab4fcafb1bd7389354dfd531e
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-01-24 16:49:43 +0300

    Task_4

--- src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
+++ src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
@@ -1,18 +1,56 @@
 package ru.otus.module1.DataCollection1
-/*
-class BallsExperiment {
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
+import scala.util.Random
+
+// Вариант решения:
+// class BallsExperiment {
+//   val collection: List[Int] = List[Int](1, 1, 1, 0, 0, 0)
+//   val iterator: Iterator[Int] = Random.shuffle(collection).iterator
+
+//   def isAnyWhiteInTwoPicks(): Boolean = {
+//     iterator.next() == 1 || iterator.next() == 1
+//   }
+// }
+
+class BallsExperiment {
+  val collection: List[Int] = List[Int](1, 1, 1, 0, 0, 0)
+
+  def getRandomIndex(): Int =
+    Random.nextInt(collection.size)
+
+  def isAnyWhiteInTwoPicks(): Boolean = {
+    val firstIndx = getRandomIndex()
+    var secondIndx = getRandomIndex()
+
+    while (firstIndx == secondIndx)
+      secondIndx = getRandomIndex()
+
+    collection(firstIndx) == 1 || collection(secondIndx) == 1
   }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
-    val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
-    val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
-    println(countOfPositiveExperiments / count)
+    val countOfExperiments = 10000
+
+    val listOfExperiments: List[BallsExperiment] =
+      List.fill(countOfExperiments)(BallsExperiment())
+
+    val countOfPositiveExperiments: Float =
+      listOfExperiments.map(_.isAnyWhiteInTwoPicks()).count(_ == true)
+
+
+    // Если просят хотя бы один белый после двух попыток достать:
+    // Б+Б: (3/6) * (2/5)  \
+    // Ч+Б: (3/6) * (3/5)  | = 0.2 + 0.3 + 0.3 = 0.8 \
+    // Б+Ч: (3/6) * (3/5)  /                         | 0.8 / (0.2 + 0.8) = 0.8
+    // Ч+Ч: (3/6) * (2/5)                      = 0.2 /
+    //
+    // Ответ должен быть 0.8
+
+    println(
+      "Теория: 0.8, расчёты " + countOfPositiveExperiments / countOfExperiments
+    )
+
   }
-}*/
+}
