b5c555df3ab27b6c1910951bc0c92cf73561a2bc
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-02-19 13:56:58 +0300

    lesson_10v2

--- src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
+++ src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
@@ -1,11 +1,29 @@
 package ru.otus.module2
 
-object homework_hkt_implicits{
+object homework_hkt_implicits {
 
-  trait Bindable[F[_], A]{
+  trait Bindable[F[_], A] {
     def map[B](f: A => B): F[B]
+
     def flatMap[B](f: A => F[B]): F[B]
   }
 
-  def tuplef[F[_], A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
+  implicit def im1[A](a: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+    override def map[B](f: A => B): Option[B] = a.map(f)
+
+    override def flatMap[B](f: A => Option[B]): Option[B] = a.flatMap(x => f(x))
+  }
+
+  implicit def im2[A](a: List[A]): Bindable[List, A] = new Bindable[List, A] {
+    override def map[B](f: A => B): List[B] = a.map(f)
+
+    override def flatMap[B](f: A => List[B]): List[B] = a.flatMap(x => f(x))
+  }
+
+  def tupleF[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
+    fa.flatMap(a => fb.map(b => (a, b)))
+
+  // Применение:
+  val r1 = tupleF(List("text"), List(1, 2, 3))
+  val r2 = tupleF(Some("text"), Some(3))
 }
