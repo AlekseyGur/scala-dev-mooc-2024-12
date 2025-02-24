25fa0ae99b3511c9001fb30415a31080e4b525d8
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-02-24 18:59:27 +0300

    lesson_10v3

--- src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
+++ src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
@@ -20,10 +20,36 @@ object homework_hkt_implicits {
     override def flatMap[B](f: A => List[B]): List[B] = a.flatMap(x => f(x))
   }
 
+  implicit def im3[A](a: Some[A]): Bindable[Some, A] = new Bindable[Some, A] {
+    override def map[B](f: A => B): Some[B] = Some(a.value.asInstanceOf[B])
+
+    override def flatMap[B](f: A => Some[B]): Some[B] = f(a.value).asInstanceOf[Some[B]]
+  }
+
   def tupleF[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
     fa.flatMap(a => fb.map(b => (a, b)))
 
+    
   // Применение:
   val r1 = tupleF(List("text"), List(1, 2, 3))
   val r2 = tupleF(Some("text"), Some(3))
+
+  println(r1)
+  println(r2)
+
+
+  // Второй вариант:
+  def tupleFi[F[_], A, B](a: F[A], b: F[B])(implicit
+      m1: F[A] => Bindable[F, A],
+      m2: F[B] => Bindable[F, B]
+  ): F[(A, B)] =
+    m1(a).flatMap(a => m2(b).map(b => (a, b)))
+
+  val r4 = tupleFi(List("text"), List(1, 2, 3))
+  val r5 = tupleFi(Some("text"), Some(3))
+  val r6 = tupleFi(Some("text"), Some(3))
+
+  println(r4)
+  println(r5)
+  println(r6)
 }
